952577e54fbc863d2af20bd46ba6e58f9aaac801
Ruben van het Kaar <rubenvhkaar@gmail.com>
2018-04-07 16:02:51 +0200

    rubens commit

--- H11-heat-equation/src/heat-equation.cxx
+++ H11-heat-equation/src/heat-equation.cxx
@@ -4,7 +4,7 @@
 #define PRINT_EXPRESSION(expr) std::cout << #expr << ": " << (expr) \
                                          << " (type: " << typeid(expr).name() << ")" << std::endl
 
-//4.  Create a class template `Number` with one public `const` attribute `value` of type `T`
+// PART 1 ###################################################################
 template <typename T>
 class Vector
 {
@@ -29,22 +29,65 @@ class Vector
     }
 
 
-
   private:
     T * elements;
     int size;
 };
 
-//5.  Create a [Fibonacci number] generator with templates but without functions
+// PART 2 ###################################################################
+template <typename T>
+class Matrix {
+private:
+	T* data;
+	int rows, columns, a, b;
+	double x;
+public:
+		Matrix() //default construct
+    	: data(nullptr), rows(0), columns(0) {} 
 
+    	Matrix(int rows, int columns) //dimensions specified
+    	: data(new T[rows, columns]), rows(rows), columns(columns) {}
+		
+		// Attributes
+		// Initialize elements
+		//double 
+		//Matrix[{a,b}] = 
+
+};
+
+// Matrix vector multiplication function
+// double MVmult(const Matrix& A, const Vector& b)
+// {
+//		int rows = b.length;
+	//	int columns = A.columns
+      //  double x=0;
+        //for (auto i=0; i<rows; i++)
+			//for(auto j=0; j<columns; j++)
+				//x[i] += A[i,j]*b[j];
+      //  return x;
+//}
+
+
+// PART 3 ###################################################################
+    //template<typename T>
+    //int cg(
+    //    const Matrix<T> &A, const Vector<T> &b, Vector<T> &x, T tol, I maxiter)
+    //{
+	
+    //}
+
+// MAIN #####################################################################
 int main()
 {
-
     std::cout << "test" << std::endl;
     Vector<int> a = 5;
     // Vector<int> c = { 1, 2, 3, 4 };
     int b[] = { 1, 2, 3, 4 };
     Vector<int> c = b;
     // std::cout << b[3] << std::endl;
-    return 0;
+	Matrix<double> M(10,20);
+	//M[{1,1}] = 69;
+return 0;
 }
+
+
