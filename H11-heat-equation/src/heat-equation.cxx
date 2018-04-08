7a97ac427341ee9ef525fd7dfb9f15f5ccb70955
Bart Ziengs <a.h.ziengs@student.tudelft.nl>
2018-04-08 20:56:48 +0200

    Added stuff

--- H11-heat-equation/src/heat-equation.cxx
+++ H11-heat-equation/src/heat-equation.cxx
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <memory>
 #include <typeinfo>
-// #define PRINT_EXPRESSION(expr) std::cout << #expr << ": " << (expr) \
-//                                          << " (type: " << typeid(expr).name() << ")" << std::endl
+#include <stdexcept>
 
-//4.  Create a class template `Number` with one public `const` attribute `value` of type `T`
 template <typename T>
 class Vector
 {
 public:
+  T *elements;
+  int size;
   // empty constructor
   Vector()
       : elements(nullptr),
@@ -40,28 +40,134 @@ public:
     }
   }
 
-  //move constructor
+  //move constructor, callable with std::move
   Vector(Vector &&v)
       : size(v.size),
         elements(v.elements)
   {
-    v.elements = 0;
-    v.data = nullptr;
+    v.size = 0;
+    v.elements = nullptr;
   }
 
-private:
-  T *elements;
-  int size;
-};
+  //copy assignment that calls copy constructor
+  Vector &operator=(const Vector &other)
+  {
+    if (this != &other)
+    {
+      delete[] elements;
+      size = other.size;
+      elements = new T[other.size];
+      for (auto i = 0; i < other.size; i++)
+        elements[i] = other.elements[i];
+    }
+    return *this;
+  }
 
-//5.  Create a [Fibonacci number] generator with templates but without functions
+  //Move assignment, call with std::move
+  Vector &operator=(Vector &&other)
+  {
+    if (this != &other)
+    {
+      delete[] elements;
+      size = other.size;
+      elements = other.elements;
+      other.size = 0;
+      other.elements = nullptr;
+    }
+    return *this;
+  }
+
+  //minus operator, automatically determines return type
+  template <typename U>
+  auto operator-(const Vector<U> &other)
+  {
+    if (size != other.size)
+      throw std::invalid_argument("Vectors are of unequal length");
+
+    auto rtype = elements[0] - other.elements[0];
+    Vector<decltype(rtype)> res(size);
+    for (auto i = 0; i < size; i++)
+      res.elements[i] = elements[i] - other.elements[i];
+
+    return res;
+  }
+
+  //addition operator, automatically determines return type
+  template <typename U>
+  auto operator+(const Vector<U> &other)
+  {
+    if (size != other.size)
+      throw std::invalid_argument("Vectors are of unequal length");
+
+    auto rtype = elements[0] + other.elements[0];
+    Vector<decltype(rtype)> res(size);
+    for (auto i = 0; i < size; i++)
+      res.elements[i] = elements[i] + other.elements[i];
+
+    return res;
+  }
+
+  //scalar multiplication (still incomplete, workds only for same types)
+  Vector<T> &operator*=(T scalar)
+  {
+    for (auto i = 0; i < size; i++)
+      elements[i] *= scalar;
+    return *this;
+  }
+
+
+  //dot implementation, other than in descripton but shorter and functionally equivalent
+  T dot(const Vector<T> &v){
+    if (size != v.size)
+      throw std::invalid_argument("Vectors are of unequal length");
+    T res = 0;
+    for(int i = 0; i < size; i++)
+    {
+      res += elements[i]*v.elements[i];
+    }
+    return res;
+  }
+
+  //Print function, for debugging purposes
+  void info()
+  {
+    std::cout << "Length of vector:  " << size << std::endl;
+    std::cout << "Entries: [";
+    for (int i = 0; i < size - 1; i++)
+    {
+      std::cout << elements[i] << ", ";
+    }
+    std::cout << elements[size - 1] << "]" << std::endl;
+  }
+};
 
 int main()
 {
 
   std::cout << "test" << std::endl;
-  Vector<int> a(4);
+  Vector<int> a = {2, 4, 6, 122};
+  Vector<int> V(3);
+  Vector<double> doubleVector = {1.5, 2.5, 3.5, 4.5};
+  Vector<double> anotherDouble(doubleVector);
+  // auto e = std::move(a);
+  // a.info();
   Vector<int> b = {1, 2, 3, 4};
-  Vector<int> c = b;
+  Vector<int> c(b); //copy assignment
+  // Vector<int> d(std::move(b)); //move assignment
+  // c.info();
+  auto n = a - doubleVector;
+  // n.info();
+  auto w = a + b;
+  // w.info();
+  auto res = a + doubleVector;
+  // res.info();
+  b*= 6.1;
+  b.info();
+  auto z = 4 * 5.6;
+  std::cout << z << std::endl;
+
+  int y = a.dot(c);
+  std::cout << y << std::endl;
+
   return 0;
 }
